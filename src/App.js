1fa484528c91cb19294ea1e3a1d66f71f059539f
Maxwel Ochieng <64557216+devmaxwel@users.noreply.github.com>
2021-09-21 00:12:45 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Login from './Components/First-Project/Login'
+
+function App() {
+  return (
+    <div >
+      
+     <Login />
+   
+    </div>
+  );
+}
+
+export default App;
